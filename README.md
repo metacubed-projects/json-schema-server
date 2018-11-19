d1f0ed6ee0aa13913df378f4b52e084204a7a498
Pushkaraj Shirgaonkar <7028125+metacubed@users.noreply.github.com>
2018-11-18 23:10:32 -0800

    Initial project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# JSON Schema Server
+Plugins to compose and serve complex JSON schemas
+
+[![Build Status](https://travis-ci.org/metacubed-projects/json-schema-server.svg?branch=master)](https://travis-ci.org/metacubed-projects/json-schema-server)
---
+++ pom.groovy
@@ -0,0 +1,16 @@
+project {
+    modelVersion '4.0.0'
+
+    name 'JSON Schema Server'
+
+    parent {
+        groupId 'org.metacubed'
+        artifactId 'root'
+        version '1.0.0'
+        relativePath ''
+    }
+
+    artifactId 'json-schema-server'
+    version '1.0.0-SNAPSHOT'
+    packaging 'pom'
+}
